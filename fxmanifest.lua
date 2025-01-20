2a37d32e5d70bb337acdb0aa71d35d06899a8e83
Antonio <antonioantonelli7@gmail.com>
2025-01-20 12:41:12 +0100

    added debug dump of markers in server
    If you don't want this, just delete it, i don't care :)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,14 @@ version '2.0.0'
 
 lua54 'yes'
 
+shared_scripts {
+    "config.lua"
+}
+
 client_scripts {
-    'config.lua',
     'client/**/*.lua'
+}
+
+server_scripts {
+    'server/**/*.lua'
 }
